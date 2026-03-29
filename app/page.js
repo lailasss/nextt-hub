fc4b3523c9089cad902d9cbe005738c4b68d5d88
lailasss <metalailass@gmail.com>
2026-03-29 15:29:42 -0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,4 @@
+'use client'
+import dynamic from 'next/dynamic'
+const App = dynamic(() => import('../components/App'), { ssr: false, loading: () => null })
+export default function Home() { return <App /> }
